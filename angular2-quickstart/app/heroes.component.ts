885bca7a621633984c24f079b5abe03bd4580a76
Sofia92 <1157335914@qq.com>
2016-04-09 13:46:29 +0800

    Angular 2 QuickStart

---
+++ angular2-quickstart/app/heroes.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import { Router } from 'angular2/router';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
+})
+
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+	
+	ngOnInit() {
+		this.getHeroes();
+  };
+
+  constructor(private _router: Router,private _heroService: HeroService) { };
+  onSelect(hero: Hero) { this.selectedHero = hero; };
+	getHeroes() {
+		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  };
+  gotoDetail() {
+    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
+
+}
+
+
+
+
+
